9e395038954f87a1dd5f3fff2434c7bbbfcf67a8
Raisa Primerova <riyuhide@gmail.com>
2020-02-03 16:46:35 -0700

    Add get endpoints for all projects

--- server.js
+++ server.js
@@ -14,7 +14,17 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3001);
+
+app.get('/api/v1/projects', async (request, response) => {
+  try {
+    const projects = await database('projects').select();
+    response.status(200).json(projects);
+  }
+  catch(error) {
+    response.status(500).json({ error });
+  }
+});
 
 app.listen(app.get('port'), () => {
   console.log('running');
