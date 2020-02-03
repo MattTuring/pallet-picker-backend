6b2bfbe7c1f7c2609ec076829a4a3a9db057c60e
Raisa Primerova <riyuhide@gmail.com>
2020-02-03 16:43:16 -0700

    Add body parser to server

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const cors = require('cors')
 const app = express();
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 app.use(express.json())
 app.use(cors())
-
 
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
+
 app.set('port', process.env.PORT || 3000);
 
 app.listen(app.get('port'), () => {
   console.log('running');
-})
+});
