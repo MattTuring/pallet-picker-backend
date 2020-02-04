b83f54fce7801673bfa217963f7b3b88749a76bf
M <m@Ms-MacBook-Pro.local>
2020-02-03 17:23:55 -0700

    Add pallet table and seed

---
+++ data/pallets.js
@@ -0,0 +1,19 @@
+const pallets = [{
+  name: 'Warm Weather',
+  color1: '#DAF7A6',
+  color2: '#FFC300',
+  color3: '#FF5733',
+  color4: '#C70039',
+  color5: '#581845',
+  project_id: 11
+}, {
+  name: 'Cold Weather',
+  color1: '#DAF7A5',
+  color2: '#FFC301',
+  color3: '#FF5732',
+  color4: '#C70038',
+  color5: '#581844',
+  project_id: 11
+}];
+
+module.exports = pallets;
--- db/seeds/dev/projects.js
+++ db/seeds/dev/projects.js
@@ -1,14 +1,21 @@
 const projectsData = require('../../../data/projects');
+const palletsData = require('../../../data/pallets');
 
 exports.seed = async (knex) => {
   try {
     await knex('projects').del();
+    await knex('pallets').del();
 
     let projectsPromises = projectsData.map(project => {
       return knex('projects').insert(project);
     });
 
-    return Promise.all(projectsPromises);
+    let palletsPromises = palletsData.map(pallet => {
+      return knex('pallets').insert(pallet);
+    });
+
+    let all = [...projectsPromises, ...palletsPromises]
+    return Promise.all(all);
   }
   catch (error) {
     console.log(`Error seeding data: ${error}`);
