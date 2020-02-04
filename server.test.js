7c4aafbe1f5dd7e142e90f2e3b7ca2dbc89ecf71
Raisa Primerova <riyuhide@gmail.com>
2020-02-04 15:07:55 -0700

    Test GET endpoint for a single project

--- server.test.js
+++ server.test.js
@@ -26,5 +26,31 @@ describe('Server', () => {
         expect(projects).toEqual(expectedProjects);
       });
     });
+
+    describe('GET /api/v1/projects/:id', () => {
+      it('should return a 200 and a single project if the project exists', async () => {
+        // setup
+        const receivedProject = await database('projects').first();
+        const { id } = receivedProject;
+        const expectedProject = JSON.parse(JSON.stringify(receivedProject))
+
+        // execution
+        const res = await request(app).get(`/api/v1/projects/${id}`);
+        const result = res.body;
+
+        // expectation
+        expect(res.status).toBe(200);
+        expect(result).toEqual(expectedProject);
+      });
+
+      it('should return a 404 and the message "Could not find project with id"', async () => {
+        const invalidID = -1;
+
+        const response = await request(app).get(`/api/v1/projects/${invalidID}`);
+
+        expect(response.status).toBe(404);
+        expect(response.body.error).toEqual(`Could not find project with id ${invalidID}`);
+      });
+    });
   });
 });
