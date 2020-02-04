2d4737f9c59a660fc57c2eabbaa61d4016690f2d
M <m@Ms-MacBook-Pro.local>
2020-02-04 14:01:18 -0700

    Improved File Structure

--- db/migrations/20200203152217_projects-table.js
+++ db/migrations/20200203152217_projects-table.js
@@ -5,20 +5,6 @@ exports.up = function(knex) {
       table.string('name');
       table.timestamps(true, true);
     })
-
-    .createTable('palettes', function (table) {
-      table.increments('id').primary();
-      table.string('name');
-      table.string('color1');
-      table.string('color2');
-      table.string('color3');
-      table.string('color4');
-      table.string('color5');
-      table.integer('project_id')
-      table.foreign('project_id').references('projects.id')
-      table.timestamps(true, true);
-    })
-
 };
 
 exports.down = function(knex) {
