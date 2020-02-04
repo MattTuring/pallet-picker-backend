eadfedc68fc8b6109a1a88759078d546574494e6
Raisa Primerova <riyuhide@gmail.com>
2020-02-03 17:06:25 -0700

    Add get endpoint for single project

--- server.js
+++ server.js
@@ -26,6 +26,22 @@ app.get('/api/v1/projects', async (request, response) => {
   }
 });
 
+app.get('/api/v1/projects/:id', async (request, response) => {
+  try {
+    const projects = await database('projects').where('id', request.params.id).select();
+    const [ project ] = projects;
+    if (!projects.length) {
+      return response.status(404).json({
+        error: `Could not find project with id ${request.params.id}`
+      });
+    }
+    return response.status(200).json(project);
+  }
+  catch(error) {
+    response.status(500).json({ error });
+  }
+});
+
 app.listen(app.get('port'), () => {
   console.log('running');
 });
