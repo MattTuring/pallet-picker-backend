8923a2a0a688996cc378dae97ac4d47226947ff3
Raisa Primerova <riyuhide@gmail.com>
2020-02-04 15:06:41 -0700

    Add test file with settings

---
+++ server.test.js
@@ -0,0 +1,13 @@
+const request = require('supertest');
+const app = require('./server');
+
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+describe('Server', () => {
+  beforeEach(async () => {
+    await database.seed.run();
+  });
+
+});
