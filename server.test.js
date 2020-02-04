b8968c28eff9a26ebb2ddbecb9fe04b8f720fcf9
Raisa Primerova <riyuhide@gmail.com>
2020-02-04 15:07:31 -0700

    Test GET endpoint for all projects

--- server.test.js
+++ server.test.js
@@ -10,4 +10,21 @@ describe('Server', () => {
     await database.seed.run();
   });
 
+  describe("Projects", () => {
+    describe('GET /api/v1/projects', () => {
+      it('should return a 200 and all of the projects', async () => {
+        // setup
+        const receivedProjects = await database('projects').select();
+        const expectedProjects = JSON.parse(JSON.stringify(receivedProjects));
+
+        // execution
+        const res = await request(app).get('/api/v1/projects');
+        const projects = res.body;
+
+        // expectation
+        expect(res.status).toBe(200);
+        expect(projects).toEqual(expectedProjects);
+      });
+    });
+  });
 });
