6ae62728654299063cfde9389d72ca2f6f7a6ded
M <m@Ms-MacBook-Pro.local>
2020-02-05 19:55:43 -0700

    Add unique

--- db/migrations/20200203152217_projects-table.js
+++ db/migrations/20200203152217_projects-table.js
@@ -2,12 +2,13 @@ exports.up = function(knex) {
   return knex.schema
     .createTable('projects', function (table) {
       table.increments('id').primary();
-      table.string('name');
+      table.string('name').unique();
       table.timestamps(true, true);
     })
 };
 
 exports.down = function(knex) {
   return knex.schema
+      .dropTable('palettes')
       .dropTable('projects')
 };
